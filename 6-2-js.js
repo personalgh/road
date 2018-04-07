bff0295690806450870ccd41a6b3b1cfdd980eb0
Personal_GH <bjshizimo@163.com>
2018-04-07 17:24:25 +0800

    button效果

---
+++ 6-2-js.js
@@ -0,0 +1,18 @@
+//// JavaScript Document
+//$(document).ready(function(){
+//	$('#fstcls').click(function(){
+//		$('#Rup22_1').show;
+//		$('#Rup22_2').hide;
+//		$('#Rup22_3').hide;
+//	});
+//	$('#seccls').click(function(){
+//		$('Rup22_1').hide;
+//		$('Rup22_2').show;
+//		$('Rup22_3').hide;
+//	});
+//	$('#fstcls').click(function(){
+//		$('Rup22_1').hide;
+//		$('Rup22_2').hide;
+//		$('Rup22_3').show;
+//	});
+//});
